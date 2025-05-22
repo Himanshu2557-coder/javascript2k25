14bb40fc5a0cc190926838d737608302f62c7a26
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-22 05:50:32 +0000

    shortcirciting concepts

---
+++ shortcircuit.js
@@ -0,0 +1,13 @@
+console.log(10 && 30)
+console.log(0 && 20)
+console.log("" && null)
+console.log(40 && -0)
+console.log(40 && null)
+
+// or operator
+console.log(10 ||30)
+console.log(0 || 20)
+console.log("" || null)
+console.log(40 || -0)
+console.log(40 || null)
+console.log(undefined || 30)
