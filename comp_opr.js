e0084ff3e69eaec3e8900ad7a224895fe107984f
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-22 06:16:28 +0000

    comparsion operators

---
+++ comp_opr.js
@@ -0,0 +1,18 @@
+// comaparison or relational operators...........
+// always give boolean results
+console.log(10<20)
+console.log(10>20)
+
+console.log(5>=6)
+console.log(9==9) 
+
+console.log("10" == 10)// abstract equality operator(check a & b are equal or we can convert it into some other type  and make them equal)
+console.log(9==7)
+console.log(9===9)
+console.log("10"===10) // strict equality operator ( checks a and b and dont convert anything)
+
+console.log(3!=3) //abstract not equal
+console.log(3!=9)
+console.log("3"!=9)
+console.log(9!==9) //strict not equal
+console.log("10"!==10)
