8b1b08e4d5727191c30b3e67d8d8dce6ccf8c598
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-22 08:17:29 +0000

    conditionals added

---
+++ conditionals.js
@@ -0,0 +1,30 @@
+console.log("Starting....")
+let userlogin = true
+let userpass = false
+let pass = true
+if(userlogin == true && userpass == true) {
+    console.log("applicable to login !!")
+}
+else if(pass = true) {
+    console.log("hey super user welcome to HOTSTAR !")
+}
+else{
+    console.log("show homepage")
+}
+console.log("END....")
+
+
+let isUserSubscribed = true
+let userhasliongateplay = false
+let userhassony = true
+if(isUserSubscribed){
+    if(userhasliongateplay){
+        console.log("watch jon wick")
+    }
+    elseif(userhassony){
+        console.log("play guardinas")
+    }
+    else{
+        console.log("play panchayat")
+    }
+}
