e4a43d70f91c57cb281a2d4c510ac408439b7243
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-23 09:20:50 +0000

    ok

---
+++ jsnew/example of if else.js
@@ -0,0 +1,30 @@
+let x = 34
+if(x%2 ==0){
+    console.log("even hai")
+}
+else{
+    console.log("odd")
+}
+
+console.log("____________________________________-")
+
+let age = 19
+if(age >= 18){
+    console.log("you can vote")
+}
+else{
+    console.log("you cannot vote")
+}
+
+console.log("__________________________________")
+let a =10 ,b =20 , c =30
+if(a<b){
+    console.log("smallest number is a")
+}
+else if(b<c){
+    console.log("smalest is b")
+}
+else{
+    console.log("smallest is c")
+}
+
