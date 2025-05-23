940f9f02770bf0af4cac4f20abad6160cab1711d
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-23 08:28:27 +0000

    done

--- jsnew/functions7.js
+++ jsnew/functions7.js
@@ -1,2 +1,17 @@
 // IN JS  FUNCTIONS ARE FIRST CLASS CITIZEN
 // IN JS FUNCTIONS CAN BE RETUREND FROM A FXN AND FXN CAN BE PASSED AS AN ARGUMENT TO ANOTHER FXN
+function somefunction(){
+    return function x(){
+        return 10
+    }
+}
+console.log("___________________________--")
+
+function anotherfunction(f1){
+    f1()
+    console.log("called f1")
+}
+function hello(){
+    console.log(`hello`)
+}
+anotherfunction(hello)
