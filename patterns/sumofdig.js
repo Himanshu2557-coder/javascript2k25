a627fc5e22b601e2e7a15f567d13dbf9dec7c1b9
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-23 10:23:58 +0000

    okie

---
+++ patterns/sumofdig.js
@@ -0,0 +1,11 @@
+function sumofdigits(x){
+    let sum =0 
+    while(x>0){
+        let lastdigit = x%10
+        sum += lastdigit
+        x = Math.floor(x/10)
+        
+    }
+    return sum
+}
+console.log(sumofdigits(12345))
