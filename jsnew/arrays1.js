c5f43f85407eb2ed17b0683a2e03de660bbb3574
himanshusainidtu <himanshusainidtu@gmail.com>
2025-05-23 09:05:55 +0000

    ok

---
+++ jsnew/arrays1.js
@@ -0,0 +1,10 @@
+let arr1 = []
+let arr2 = ['sanket ', true , 100 ,"def" ,null ,-1]
+console.group(arr1 ,arr2)
+console.log(arr2[3])
+console.log(arr2[10])
+
+
+// auto add by displaying 4 items empty
+arr2[10] = 'js'
+console.log(arr2)
